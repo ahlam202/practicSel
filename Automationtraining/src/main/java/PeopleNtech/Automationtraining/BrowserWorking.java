5fbd4990fc77b53872585fbcd2ac896732b8672a
Admin <Admin@DESKTOP-3KUG7HD>
2021-11-23 21:40:18 -0600

    first time checking

---
+++ Automationtraining/src/main/java/PeopleNtech/Automationtraining/BrowserWorking.java
@@ -0,0 +1,30 @@
+package PeopleNtech.Automationtraining;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserWorking {
+
+	static WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+        // how to open the browser
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\Automationtraining\\src\\Drivers\\chromedriver.exe");
+	driver=new ChromeDriver();
+	driver.navigate().to("https://www.amazon.com/");
+	driver.manage().window().maximize();
+	Thread.sleep(6000);// thread is compailer
+	driver.navigate().refresh();
+	Thread.sleep(6000);
+	driver.navigate().back();
+	Thread.sleep(6000);
+	driver.navigate().forward();
+	Thread.sleep(6000);
+	String windowhandle=driver.getWindowHandle();
+	System.out.println(windowhandle);
+	System.out.println(driver.getCurrentUrl());
+	driver.close();
+	}
+
+}
